d099057e8701ffaec659795460c94ba59b32f3cd
Brendan Dahl <brendan.dahl@gmail.com>
2025-11-05 19:56:57 +0000

    Enable webpack production builds.
    This seems to be working correctly now.

--- resources/transformers-js/webpack.prod.js
+++ resources/transformers-js/webpack.prod.js
@@ -1,18 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = merge(common, {
-    // mode: "production", // TODO production isn't working???
-    mode: "development",
+    mode: "production",
     devtool: "source-map",
-    plugins: [
-    ],
-    module: {
-    },
-    // optimization: {
-    //     minimize: true,
-    //     minimizer: [new TerserPlugin()],
-    // },
 });
