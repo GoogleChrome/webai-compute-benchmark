f26d5eebfe1ef03f9381bb567c632be4084cc38a
Brendan Dahl <brendan.dahl@gmail.com>
2026-01-28 18:31:48 +0000

    limit the tests to all wasm and 2 gpu tests

--- tests/run-end2end.mjs
+++ tests/run-end2end.mjs
@@ -9,11 +9,23 @@ This script runs end2end tests by invoking the benchmark via the main
 Speedometer page in /index.html.
 `.trim();
 
-const ONE_MINUTE_IN_MS = 60000
+const ONE_MINUTE_IN_MS = 60000;
 
 const { driver, PORT, stop } = await testSetup(HELP);
 
-const suites = benchmarkConfigurator.suites;
+// Running all of the benchmarks is very slow (especially when the GPU is emulated). To run the
+// tests faster we run all of the Wasm benchmarks, and only a few GPU tests to cover most of
+// the common code. To run all benchmarks, enable this.
+const RUN_FULL_SUITE = false;
+let tags = 'wasm,gpu-test-suite';
+let suites = benchmarkConfigurator.suites.filter(suite => suite.tags.some((tag) => tag === 'wasm' || tag === 'gpu-test-suite'));
+let timeout = 5 * ONE_MINUTE_IN_MS;
+
+if (RUN_FULL_SUITE) {
+    tags = 'all';
+    suites = benchmarkConfigurator.suites;
+    timeout = 20 * ONE_MINUTE_IN_MS;
+}
 
 async function testPage(url) {
     console.log(`Testing: ${url}`);
@@ -74,7 +86,7 @@ function validateMetric(name, metric) {
 
 async function testIterations() {
     const iterationCount = 2;
-    const metrics = await testPage(`index.html?iterationCount=${iterationCount}`);
+    const metrics = await testPage(`index.html?iterationCount=${iterationCount}&tags=${tags}`);
     suites.forEach((suite) => {
         if (suite.enabled) {
             const metric = metrics[suite.name];
@@ -89,7 +101,7 @@ async function testIterations() {
 }
 
 async function testAll() {
-    const metrics = await testPage("index.html?iterationCount=1&tags=all");
+    const metrics = await testPage(`index.html?iterationCount=1&tags=${tags}`);
     suites.forEach((suite) => {
         assert(suite.name in metrics);
         const metric = metrics[suite.name];
@@ -114,7 +126,7 @@ async function testDeveloperMode() {
 
 async function test() {
     try {
-        await driver.manage().setTimeouts({ script: 20 * ONE_MINUTE_IN_MS });
+        await driver.manage().setTimeouts({ script: timeout });
         await testIterations();
         await testAll();
         await testDeveloperMode();
